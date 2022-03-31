7b4330b11f5d7b9f5a46ea2cbcaadb43316f2970
julienmalard <julien.malard@mail.mcgill.ca>
2022-03-31 16:43:51 +0700

    បែបនេះ

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,16 +1,9 @@
 const execa = import('execa')
-const emoji = import('node-emoji')
-const chalk = import('chalk')
-
-const firstLog = `${emoji.get('fast_forward')} ${chalk.yellow('On compile...')}`
-const secondLog = `${emoji.get('fast_forward')} ${chalk.yellow('On publie...')}`
-const thirdLog = `${emoji.get('rocket')} ${chalk.green('Votre site est publié !')} ${emoji.get('rocket')}`
 
 ;(async () => {
   try {
     const { stdout: currentBranch } = await execa.command('git branch --show-current')
     await execa.command('git checkout --orphan gh-pages')
-    console.log(firstLog)
     await execa.command('yarn build', { stdio: 'inherit' })
 
     // Idée de https://stackoverflow.com/questions/48521177/404-when-reloading-a-vue-website-published-to-github-pages
@@ -25,13 +18,11 @@ const thirdLog = `${emoji.get('rocket')} ${chalk.green('Votre site est publié !
 
     await execa.command('git --work-tree dist add --all')
     await execa.command('git --work-tree dist commit -m "gh-pages"')
-    console.log(secondLog)
 
     await execa.command('git push origin HEAD:gh-pages --force', { stdio: 'inherit' })
     await execa.command('rm -r dist')
     await execa.command(`git checkout -f ${currentBranch}`)
     await execa.command('git branch -D gh-pages')
-    console.log(thirdLog)
 
   } catch (e) {
     console.log(e.message)
